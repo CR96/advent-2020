cd187a048e469cf331201c069db28dfad467e758
Corey Rowe <coreyr1196@gmail.com>
2020-12-12 01:04:28 -0500

    Day 1 solution, pass 1

--- src/main/java/com/corey/advent/DayOne.java
+++ src/main/java/com/corey/advent/DayOne.java
@@ -1,7 +1,25 @@
 package com.corey.advent;
 
 public class DayOne {
+
+    static int[] expenseReport = new int[]{1721, 979, 366, 299, 675, 1456};
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println(findProduct(expenseReport));
+    }
+
+    // Find two numbers that add to 2020, then return their product
+    public static int findProduct(int[] expenseReport) {
+
+        for (int i = 0; i < expenseReport.length; i++) {
+            for (int j = 1; j < expenseReport.length; j++) {
+                if (expenseReport[i] + expenseReport[j] == 2020) {
+                    return expenseReport[i] * expenseReport [j];
+                }
+            }
+        }
+
+        // Return -1 if no result found
+        return -1;
     }
 }
